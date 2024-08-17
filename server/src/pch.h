aa9ecb83c33450733287b73a5a9fe46549f6f65d
whale.park18 <whale.park18@gmail.com>
2024-08-18 02:27:18 +0900

    build: (#4) 솔루션 구조 개선
    [수정 내용]
    - 미리컴파일된헤더 사용 안함
    - utils 프로젝트 추가

--- server/src/pch.h
+++ server/src/pch.h
@@ -7,7 +7,8 @@
 #ifndef PCH_H
 #define PCH_H
 
-// 여기에 미리 컴파일하려는 헤더 추가
-#include "framework.h"
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
 
 #endif //PCH_H
--- utils/src/pch.h
+++ utils/src/pch.h
@@ -11,4 +11,9 @@
 #define new DEBUG_NEW
 #endif
 
+#define WIN32_LEAN_AND_MEAN             // 거의 사용되지 않는 내용을 Windows 헤더에서 제외합니다.
+
+// 여기에 미리 컴파일하려는 헤더 추가
+
+
 #endif //PCH_H
